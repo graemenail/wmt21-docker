3a7e9451d435552f8342c0468ec4fa722f712643
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-08-31 00:08:13 +0100

    Add sacreblue test to build

--- minimal/build.sh
+++ minimal/build.sh
@@ -6,14 +6,12 @@ tag=jelmervdl_wmt22_minimal
 docker build \
 	--build-arg GPU_BUILD=OFF \
 	--build-arg MARIAN_CPU_ARCH=native \
-	--build-arg MARIAN_CPU_REPO=https://github.com/XapaJIaMnu/marian-dev.git \
-	--build-arg MARIAN_CPU_REF=wngt2021avx512 \
 	--build-arg MODEL=student.12-1.tiny \
 	--build-arg SRC=en \
 	--build-arg TRG=de \
 	--progress=plain \
 	-t $tag "$@" .
 
-echo "hello world!" | docker run --rm $tag -- CPU-ALL throughput
+sacrebleu -t wmt19 -l en-de --echo src | docker run -i --rm $tag CPU-ALL throughput
 
 docker save $tag | pigz -9c > $tag-$(date +'%Y%m%d%H%M%S').tar.gz
